fcca8fcc355c117ac345d7aed238198754e45f24
liushuangddu <liushuangddu@foxmail.com>
2016-06-20 13:34:27 +0800

    Initial Commit

---
+++ LSCustomView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSCustomView
+//
+//  Created by LIUS on 16/6/20.
+//  Copyright © 2016年 LIUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LSCustomViewTests/LSCustomViewTests.m
@@ -0,0 +1,39 @@
+//
+//  LSCustomViewTests.m
+//  LSCustomViewTests
+//
+//  Created by LIUS on 16/6/20.
+//  Copyright © 2016年 LIUS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LSCustomViewTests : XCTestCase
+
+@end
+
+@implementation LSCustomViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
